b1f9d51d37cb60992ad8e8dd4c7363e37ba52e8c
Vladislav Stoitsov <vlados.01@gmail.com>
2017-11-01 22:43:50 +0200

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { YagaModule, OSM_TILE_LAYER_URL }   from '@yaga/leaflet-ng2';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+    template: `<yaga-map><yaga-tile-layer [(url)]="tileLayerUrl"></yaga-tile-layer></yaga-map>`,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+    public tileLayerUrl: string = OSM_TILE_LAYER_URL;
   title = 'app';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { YagaModule } from '@yaga/leaflet-ng2';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +9,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+        YagaModule,
+		BrowserModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
